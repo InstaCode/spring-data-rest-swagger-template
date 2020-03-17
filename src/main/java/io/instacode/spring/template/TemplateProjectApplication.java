8f2453ce7256b61329dd039c7521a4c322e5a309
Jason Edstrom <JasonE@magenic.com>
2020-03-17 14:39:33 -0500

    Missing pieces for successful build

--- src/main/java/io/instacode/spring/template/TemplateProjectApplication.java
+++ src/main/java/io/instacode/spring/template/TemplateProjectApplication.java
@@ -2,8 +2,11 @@ package io.instacode.spring.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
 @SpringBootApplication
+@Import(SpringDataRestConfiguration.class)
 public class TemplateProjectApplication {
 
   public static void main(String[] args) {
